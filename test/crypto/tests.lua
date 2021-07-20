34eff057a23aa1aa5afe0e6cdfe0a6710ecdce54
Francisco Castro <fcr@adinet.com.uy>
2021-07-20 09:19:15 -0300

    Adding AES keyschedule

--- test/crypto/tests.lua
+++ test/crypto/tests.lua
@@ -3,6 +3,7 @@ local oo = require 'lualib.oo'
 
 
 local Tests = oo.class(BaseTest)
+  :link_class(require 'test.crypto.aes')
   :link_class(require 'test.crypto.rsa')
   :link_class(require 'test.crypto.sha1')
 
