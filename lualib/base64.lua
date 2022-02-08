25f4a22890f3c7b4efea44e14d488af1294d5671
Francisco Castro <fcr@adinet.com.uy>
2022-02-08 04:22:21 -0300

    adding decode to base64

--- lualib/base64.lua
+++ lualib/base64.lua
@@ -5,11 +5,12 @@ local BASE64URL_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0
 
 local default_options = {
   charset = BASE64_CHARSET,
+  clean_spaces = true,
 }
 
 
 local function gen_character_map(charset)
-  local res = {}
+  local res = {inv={}}
   if #charset == 65 then
     res.padding = charset:sub(65, 65)
   elseif #charset == 64 then
@@ -19,7 +20,7 @@ local function gen_character_map(charset)
   end
   for i = 0, 63 do
     local c = charset:sub(i+1, i+1)
-    res[c] = i
+    res.inv[c:byte()] = i
     res[i] = c
   end
   return res
@@ -70,8 +71,66 @@ local function encode(input, options)
 end
 
 
+local function decode(input, options)
+  options = options or default_options
+  local charset_map = charset_maps[options.charset or default_options.charset]
+
+  local clean_spaces = default_options.clean_spaces
+  if options.clean_spaces ~= nil then clean_spaces = default_options.clean_spaces end
+  -- we run find and gsub separately to avoid creating intermediate strings
+  if clean_spaces and input:find '%s' then
+    input = input:gsub('%s', '')
+  end
+
+  -- input_len is the offset of the last non-padding char
+  local input_len = #input
+  local padding_len = 0
+  if charset_map.padding ~= '' then
+    assert(#input % 4 == 0, 'input length must be a multiple of 4')
+    -- find returns index from the beginning of the string to the position of the first padding char,
+    -- that's why we substract 1 from the total and add 1 to the default:
+    input_len = (input:find(charset_map.padding, -2, true) or input_len+1)-1
+    padding_len = #input - input_len
+  else
+    -- how many bytes are needed until the length is a multiple of 4
+    padding_len = bit.band(input_len, -4) - input_len
+    assert(padding_len < 3, 'invalid padding amount')
+  end
+
+  local inv = charset_map.inv
+  local res = {}
+  for i = 1, input_len-3, 4 do
+    local a1, a2, a3, a4 = input:byte(i, i+3)
+    local b1, b2, b3, b4 = inv[a1], inv[a2], inv[a3], inv[a4]
+    res[#res+1] = string.char(
+      bit.bor(bit.lshift(b1, 2), bit.rshift(b2, 4)),
+      bit.bor(bit.lshift(bit.band(b2, 15), 4), bit.rshift(b3, 2)),
+      bit.bor(bit.lshift(bit.band(b3, 3), 6), b4)
+    )
+  end
+
+  if padding_len == 1 then
+    local a1, a2, a3 = input:byte(input_len-2, input_len)
+    local b1, b2, b3 = inv[a1], inv[a2], inv[a3]
+    res[#res+1] = string.char(
+      bit.bor(bit.lshift(b1, 2), bit.rshift(b2, 4)),
+      bit.bor(bit.lshift(bit.band(b2, 15), 4), bit.rshift(b3, 2))
+    )
+  elseif padding_len == 2 then
+    local a1, a2 = input:byte(input_len-1, input_len)
+    local b1, b2 = inv[a1], inv[a2]
+    res[#res+1] = string.char(
+      bit.bor(bit.lshift(b1, 2), bit.rshift(b2, 4))
+    )
+  end
+
+  return table.concat(res)
+end
+
+
 return {
   encode = encode,
+  decode = decode,
   BASE64_CHARSET = BASE64_CHARSET,
   BASE64URL_CHARSET = BASE64URL_CHARSET,
 }
--- test/base64.lua
+++ test/base64.lua
@@ -4,12 +4,33 @@ local base64 = require 'lualib.base64'
 
 local Base64Test = oo.class(BaseTest)
 
+local long_string = "xe6LAPBF9BxyggKWGjwFaVGcNnHmBy5+5B/hSGitCinAmRxrIQDGmya7CGdU"
+local long_bin = (
+  "\197\238\139\0\240E\244\28r\130\2\150\26<\5iQ\1566q\230\7.~\228\31\225Hh\173\n" ..
+  ")\192\153\28k!\0\198\155&\187\8gT"
+)
+
 
 function Base64Test:test_encode()
   self:assert_equal(base64.encode '', '')
+  self:assert_equal(base64.encode 'x', 'eA==')
   self:assert_equal(base64.encode 'holañmundo', 'aG9sYcOxbXVuZG8=')
+  self:assert_equal(base64.encode(long_bin), long_string)
 end
 
+function Base64Test:test_decode()
+  self:assert_equal(base64.decode '', '')
+  self:assert_equal(base64.decode 'eA==', 'x')
+  self:assert_equal(base64.decode 'aG9sYcOxbXVuZG8=', 'holañmundo')
+  self:assert_equal(base64.decode(long_string), long_bin)
+end
+
+function Base64Test:test_reencode_range()
+  for i = 0, #long_bin do
+    local substr = long_bin:sub(1, i)
+    self:assert_equal(base64.decode(base64.encode(substr)), substr)
+  end
+end
 
 Base64Test:run_if_main()
 
