876fdac2d32f894ecd97a30e2fd53db7de97da14
Francisco Castro <fcr@adinet.com.uy>
2022-03-30 02:38:43 -0300

    Adding test for Sequence with default value

--- test/asn1.lua
+++ test/asn1.lua
@@ -22,6 +22,7 @@ function Asn1Test:test_encode()
       asn1.IA5String:new {name='name'},
       asn1.Boolean:new {name='ok'},
    }
+   self:assert_equal(asn1.Sequence:new{asn1.Integer:new{default=123}}:encode{}, '\48\3\2\1\123')
    self:assert_equal(asn1.Sequence:new{of=asn1.Integer:new{}}:encode{10, 20, 30}, '\48\9\2\1\10\2\1\20\2\1\30')
    self:assert_equal(schema:encode{name='Smith', ok=true}, '\48\10\22\5Smith\1\1\1')
    self:assert_equal(asn1.PrintableString:new{}:encode 'Hello, World1 \'()+,-./:=?', '\19\25Hello, World1 \'()+,-./:=?')
